bacc2f223c0608bb70a7d6a5d4aa0504dce0d870
UdyneO2 <dynemuhamad@gmail.com>
2025-07-11 14:58:44 +0000

    P

---
+++ working/rootdir.mk
@@ -0,0 +1,14 @@
+# Ramdisk
+PRODUCT_PACKAGES += \
+    init.oppo.sensor.rc \
+    init.qcom.factory.rc \
+    init.qcom.rc \
+    init.qcom.usb.rc \
+    init.qti.ufs.rc \
+    init.target.rc \
+    init.wlan.qcom.rc \
+    init.wlan.target.rc \
+    fstab-wlan.qcom \
+    fstab.default \
+    fstab.emmc \
+    ueventd.qcom.rc \
